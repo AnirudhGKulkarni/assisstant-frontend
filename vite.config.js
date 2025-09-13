69581a89a954f3df0b7a8334968ef2e86d018f95
AnirudhGKulkarni <aneegk1234@gmail.com>
2025-09-13 13:12:52 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/assisstant-frontend/',  // 👈 must match your repo name
-})
+  base: "./", // <-- important for deployed apps
+});
